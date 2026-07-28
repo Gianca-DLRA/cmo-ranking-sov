46d79496447ba4a6d719853283ee57221c4afeb7
Gianca-DLRA <giancarlo.dlra@gmail.com>
2026-07-28 14:26:54 -0600

    Eliminacion de script no utilizado

--- code/pull_missing_teams.py
+++
@@ -1,8 +0,0 @@
-#Import libraries
-from xmlrpc import client
-import requests as req
-import json
-from openpyxl import Workbook
-import pandas as pd
-from apify_client import ApifyClient
-from pathlib import Path
